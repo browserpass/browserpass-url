e0753c1b7eff40207d6e2e9b191adfdfe414422b
Maxim Baz <git@maximbaz.com>
2020-02-02 01:40:58 +0100

    Fix about:XXX url support in Firefox

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ module.exports = class extends URL {
      * @param string base Base URL
      */
     constructor(url, base) {
-        super(url, base);
-
-        // workaround for firefox bug
-        if (this.origin === "null") {
-            Object.defineProperty(this, "origin", {get: () => null});
+        try {
+            super(url, base);
+        } catch {
+            // For all invalid input, initialize fields with guaranteed invalid host
+            // https://serverfault.com/a/846523
+            super("http://invalid.invalid");
         }
 
         // if no port is defined, but the protocol port is known, set the port to that
